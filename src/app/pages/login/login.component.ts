1bcf96339fb3f82fa791408cb5fe0717e9e653c4
Zafer KAYIKÇILAR <zafer.kayikcilar@yazara.com>
2023-08-24 18:15:23 +0300

    minor

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -8,6 +8,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/core/services';
 import { environment } from 'src/environments/environment';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'kfn-login',
@@ -24,25 +25,34 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  submitted = false;
+
   // Injects >= Angular 14
-  private fb = inject(FormBuilder);
+  private formBuilder = inject(FormBuilder);
   private authService = inject(AuthService);
 
   // Typed Forms >= Angular 14
-  form = this.fb.nonNullable.group({
+  form = this.formBuilder.nonNullable.group({
     username: [environment.username, [Validators.required]],
     password: [environment.password, [Validators.required]],
   });
 
   login() {
-    console.log(this.form.value);
-    this.authService.login(this.form.value).subscribe({
-      next: (res) => {
-        console.log(res);
-      },
-      error: (err) => {
-        console.error(err);
-      },
-    });
+    this.submitted = true;
+    this.authService
+      .login(this.form.value)
+      .pipe(
+        finalize(() => {
+          this.submitted = false;
+        })
+      )
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+        },
+        error: (err) => {
+          console.error('Hata: ' + err);
+        },
+      });
   }
 }
