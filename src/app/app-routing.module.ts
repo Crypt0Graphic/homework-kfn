65959e6f7e1b12aebc23f1b2c46854a63003a850
Zafer KAYIKÇILAR <zafer.kayikcilar@yazara.com>
2023-08-24 15:13:19 +0300

    public & private layouts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicComponent } from './core/layouts/public/public.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PrivateComponent } from './core/layouts/private/private.component';
+import { authGuard } from './core/guards/auth.guard';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductComponent } from './pages/product/product.component';
+import { BasketComponent } from './pages/basket/basket.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PrivateComponent,
+    canActivate: [authGuard],
+    children: [
+      { path: '', component: ProductsComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'product/:id', component: ProductComponent },
+      { path: 'basket', component: BasketComponent },
+    ],
+  },
+  {
+    path: '',
+    component: PublicComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PrivateComponent } from './layouts/private/private.component';
+import { PublicComponent } from './layouts/public/public.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PrivateComponent,
+    PublicComponent
+  ],
   imports: [
     BrowserAnimationsModule,
   ]
